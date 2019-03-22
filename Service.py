fa2abc2f07f881e031f542029fe53cab2dc6e03d
skvorekn <skvorekn@miamioh.edu>
2019-03-22 18:59:24 -0400

    python app

---
+++ Service.py
@@ -0,0 +1,19 @@
+from models import ToDoModel
+
+class ToDoService:
+    def __init__(self):
+        self.model = ToDoModel()
+
+    def create(self, params):
+
+        return self.model.create(params)
+
+    def update(self, item_id, params):
+        return self.model.update(item_id, params)
+
+    def delete(self, item_id):
+        return self.model.delete(item_id)
+
+    def list(self, user_id=None):
+        response = self.model.list(user_id=user_id)
+        return response
--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 # app.py
-from flask import Flask           # import flask
+from flask import Flask, request, jsonify  # import flask
+
+from Service import ToDoService
+from models import Schema
+
+# import json
+
 app = Flask(__name__)             # create an app instance
 
-@app.route("/name")                   # at the end point /
-def hello():                      # call method hello
-    return "Hello " + name         # which returns "hello world"
-if __name__ == "__main__":        # on running python app.py
+@app.route("/todo", methods=["GET"])
+def list_todo():
+    return jsonify(ToDoService().list())
+
+@app.route("/todo", methods=["POST"])
+def create_todo():
+    return jsonify(ToDoService().create(request.get_json()))
+
+@app.route("/todo/<item_id>", methods=["PUT"])
+def update_item(item_id):
+    return ToDoService().update(item_id, request.get_json())
+
+@app.route("/todo/<item_id>", methods=["DELETE"])
+def delete_item(item_id):
+    return ToDoService().delete(item_id)
+
+if __name__ == "__main__":         # on running python app.py
+    Schema()
     app.run(debug = True)                     # run the flask app
